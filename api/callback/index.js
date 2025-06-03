f0ad63530d206291013c2ba0358abef01ecfd40b
windowsworldcartoon <165859366+windowsworldcartoon@users.noreply.github.com>
2025-06-03 01:34:23 +0000

    create all

---
+++ api/callback/index.js
@@ -0,0 +1,11 @@
+// Get the URL parameters
+const urlParams = new URLSearchParams(window.location.search);
+
+// Get the data from the URL parameters
+const data = urlParams.get('data');
+
+
+if (data) {
+    // Do something with the data
+    //
+}
